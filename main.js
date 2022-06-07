cb24b5ede6297fed60d762a8cc48a151b4a77804
L-o-j-i-p <99147215+L-o-j-i-p@users.noreply.github.com>
2022-06-07 16:02:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+function myFunction() {
+    var box1 = document.getElementById("demo")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+}
+
+function mFunction() {
+    var box1 = document.getElementById("de")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+}
+
+function Function() {
+    var box1 = document.getElementById("dem")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+}
+
+function Fnction() {
+    var box1 = document.getElementById("dm")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+}
+
+function Funtion() {
+    var box1 = document.getElementById("em")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+}
+
+function my() {
+var box1 = document.getElementById("b1")
+var div = box1
+div.style.display = div.style.display === "none"
+? "block"
+: "none"
+};
+
+function Funn() {
+var box1 = document.getElementById("bbox")
+var div = box1
+div.style.display = div.style.display === "none"
+? "block"
+: "none"
+};
+
+function Fnn() {
+var box1 = document.getElementById("bgo")
+var div = box1
+div.style.display = div.style.display === "none"
+? "block"
+: "none"
+};
+
+function Nhh(){
+    var box1 = document.getElementById("nbh")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none" 
+}
+
